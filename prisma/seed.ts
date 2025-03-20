ef9a213d8229ca3c38dd75a6b64e1367966a8e0e
iamciscoo <francisjac21@gmail.com>
2025-03-21 02:28:28 +0300

    Update seed file to use bcryptjs instead of bcrypt

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { getPexelsImages } from '../src/lib/pexels';
 
 const prisma = new PrismaClient();
